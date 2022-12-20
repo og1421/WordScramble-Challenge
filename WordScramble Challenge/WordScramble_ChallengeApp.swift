783f9722baff665762b1967794c5e866c770d334
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:27:16 -0300

    Initial Commit

---
+++ WordScramble Challenge/WordScramble_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_ChallengeApp.swift
+//  WordScramble Challenge
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
